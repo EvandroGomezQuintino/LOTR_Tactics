1f7bfac1da8d29394443ae7905b848eb0fcacadb
EvandroGomezQuintino <36605703+EvandroGomezQuintino@users.noreply.github.com>
2021-03-01 08:22:32 +0000

    Added Project to Git

---
+++ LOTR Chess/Software Development Project - LOTR Chess/Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleSystem { START, NAZGUL, HEROES, WON, LOST }
+public enum Players { PLAYER1, PLAYER2 }
+
+
+
+public class GameController : MonoBehaviour
+{
+    // ************** STEP LIST *****************
+    // DONE: Check Physics.RayCast2D to check mouse click and object interaction
+    // DONE: Organize classes for: Turn, Pieces
+    // DONE: Need to create a matrix for the board
+    // DONE: Need to map the positions 
+    // DONE: Check turns
+    // DONE: Fix TileSelected respawn
+    // DONE: Destroy TileMovement gameObjects
+    // TODO: 
+
+
+
+
+
+
+
+
+
+
+    // ************** STEP LIST *****************
+    //The game stages
+    public BattleSystem turn;
+    public Players player;
+
+    //Heroes Pieces
+    public GameObject _gandalf, aragorn, frodo, sam, merry, pippin, legolas, boromir, gimli;
+    //Nazguls
+    public GameObject witchKing, nazgul_1, nazgul_2, nazgul_3, nazgul_4, nazgul_5, nazgul_6, nazgul_7, nazgul_8;
+
+
+    //Mapping Boardgame positions X and Y
+    private Dictionary<float, int> xBoard;
+    private Dictionary<float, int> yBoard;
+
+
+    //BoardPositions
+    public GameObject[,] positions = new GameObject[14, 14];
+
+
+    //Movement
+    public GameObject objSelected;
+    private Movement _movement;
+
+    void Start()
+    {
+
+        //Board mapping
+        xBoard = new Dictionary<float, int>();
+        yBoard = new Dictionary<float, int>();
+
+
+        turn = BattleSystem.START;
+        _movement = GameObject.FindWithTag("Movement").GetComponent<Movement>();
+        //Adding delay when changing the turn
+        gameSetup();
+
+        //Delay the beginning of the first player
+        turn = BattleSystem.NAZGUL;
+        nazgulTurn();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        //TESTING IENUMERATOR IN THE MOVEMENT FUNCTION
+
+        if (Input.GetMouseButtonDown(0) && turn == BattleSystem.NAZGUL && player == Players.PLAYER1)
+        {
+            // TODO:    Add code below to movement function
+            //Tracing mouse position and returning GameObject name
+            Ray ray;
+            RaycastHit hitdata;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitdata, 20))
+            {
+
+                objSelected = hitdata.transform.gameObject;
+                Debug.LogError(objSelected.ToString());
+                Debug.LogError("PLAYER 1 - NAZGUL");
+                _movement.clickObj(objSelected);
+                
+            }
+        }
+        else if (Input.GetMouseButtonDown(0) && turn == BattleSystem.HEROES && player == Players.PLAYER2)
+        {
+            // TODO:    Add code below to movement function
+            // Tracing mouse position and returning GameObject name
+            Ray ray;
+            RaycastHit hitdata;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitdata, 20))
+            {
+
+                objSelected = hitdata.transform.gameObject;
+                Debug.LogError(objSelected.ToString());
+                Debug.LogError("PLAYER 2 - HEROES");
+                
+                _movement.clickObj(objSelected);
+            }
+        }
+
+
+    }
+
+
+    public void gameSetup()
+    {
+        loadBoardPositions();
+
+        //Mapping pieces in the boardgame and Instiating GameObjects
+        positions[xPos(_gandalf.transform), yPos(_gandalf.transform)] = Instantiate(_gandalf);
+        positions[xPos(aragorn.transform), yPos(aragorn.transform)] = Instantiate(aragorn);
+        positions[xPos(frodo.transform), yPos(frodo.transform)] = Instantiate(frodo);
+        positions[xPos(sam.transform), yPos(sam.transform)] = Instantiate(sam);
+        positions[xPos(merry.transform), yPos(merry.transform)] = Instantiate(merry);
+        positions[xPos(pippin.transform), yPos(pippin.transform)] = Instantiate(pippin);
+        positions[xPos(legolas.transform), yPos(legolas.transform)] = Instantiate(legolas);
+        positions[xPos(boromir.transform), yPos(boromir.transform)] = Instantiate(boromir);
+        positions[xPos(gimli.transform), yPos(gimli.transform)] = Instantiate(gimli);
+        positions[xPos(witchKing.transform), yPos(witchKing.transform)] = Instantiate(witchKing);
+        positions[xPos(nazgul_1.transform), yPos(nazgul_1.transform)] = Instantiate(nazgul_1);
+        positions[xPos(nazgul_2.transform), yPos(nazgul_2.transform)] = Instantiate(nazgul_2);
+        positions[xPos(nazgul_3.transform), yPos(nazgul_3.transform)] = Instantiate(nazgul_3);
+        positions[xPos(nazgul_4.transform), yPos(nazgul_4.transform)] = Instantiate(nazgul_4);
+        positions[xPos(nazgul_5.transform), yPos(nazgul_5.transform)] = Instantiate(nazgul_5);
+        positions[xPos(nazgul_6.transform), yPos(nazgul_6.transform)] = Instantiate(nazgul_6);
+        positions[xPos(nazgul_7.transform), yPos(nazgul_7.transform)] = Instantiate(nazgul_7);
+        positions[xPos(nazgul_8.transform), yPos(nazgul_8.transform)] = Instantiate(nazgul_8);
+
+
+
+        //TESTING x,y POSITIONS
+        //Debug.LogError("X:" + xPos(_gandalf));
+        //Debug.LogError("Y:" + yPos(_gandalf));
+        //Debug.LogError("-----------------");
+        //Debug.LogError("X:" + xPos(legolas));
+        //Debug.LogError("Y:" + yPos(legolas));
+        //Debug.LogError("-----------------");
+        //Debug.LogError("X:" + xPos(nazgul_4));
+        //Debug.LogError("Y:" + yPos(nazgul_4));
+        //Debug.LogError("-----------------");
+        //Debug.LogError("X:" + xPos(witchKing));
+        //Debug.LogError("Y:" + yPos(witchKing));
+        //Debug.LogError(positions[2, 2].name);
+        //Debug.LogError(positions[1, 1].name);
+        //Debug.LogError(positions[1, 2].name);
+        //Debug.LogError(positions[0, 13].name);
+        //Debug.LogError(positions[1, 12].name);
+        //Debug.LogError(positions[2, 11].name);
+
+    }
+
+
+    // Mordor Player turn
+    public void nazgulTurn()
+    {
+
+        // TODO:    Update UI Message
+        StartCoroutine(displayMessage());
+
+        // User will be able to click after here
+        player = Players.PLAYER1;
+
+
+    }
+
+
+    public void heroesTurn()
+    {
+        // TODO:    Update UI Message
+        StartCoroutine(displayMessage());
+
+        // User will be able to click after here
+        player = Players.PLAYER2;
+
+
+    }
+
+
+
+    IEnumerator displayMessage()
+    {
+        //TODO: Check turn and display players turn
+        if (turn == BattleSystem.NAZGUL)
+        {
+            //TODO: Display Mordor Turn
+            yield return new WaitForSeconds(2f);
+        }
+        else if (turn == BattleSystem.HEROES)
+        {
+            //TODO: Display LOTR Turn
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    // Loading dictionaries with World Position
+    private void loadBoardPositions()
+    {
+        for (int i = 0; i < 14; i++)
+        {
+            xBoard.Add(6.5f - i, i);
+            yBoard.Add(-6.5f + i, i);
+
+
+        }
+    }
+
+    //ATTENTION!
+    // The Trnasform.Position is different for the board position which uses [row][column]
+    // This methods conver the world position 'Y' into [row], so the game is based on the board position.
+
+    //Converts 'Y' world position into [row]
+    public int xPos(Transform obj)
+    {
+        Transform objXPosition = obj;
+        int xPosition = xBoard[objXPosition.GetComponent<Transform>().position.y];
+        return xPosition;
+    }
+
+    //Converts 'X' world position into [column]
+    public int yPos(Transform obj)
+    {
+        Transform objYPosition = obj;
+        int yPosition = yBoard[objYPosition.GetComponent<Transform>().position.x];
+        return yPosition;
+    }
+}
+
---
+++ LOTR Chess/Software Development Project - LOTR Chess/Assets/Game/Scripts/Movement.cs
@@ -0,0 +1,245 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private int actions;
+    public GameController game;
+    //public PathMoveSet moveSet;
+
+    //Tiles
+    public GameObject tileSelected;
+    public GameObject tileMovement;
+    //public GameObject tileCombat;
+
+    //Selection
+    private GameObject objSelected;
+
+    //Recording Movement
+    private GameObject previousPiece;
+
+    public void Start()
+    {
+        game = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        actions = 1;
+
+
+
+
+    }
+
+
+
+    public void clickObj(GameObject obj)
+    {
+        objSelected = obj;
+        int objXposition = game.xPos(objSelected.transform);
+        int objYposition = game.yPos(objSelected.transform);
+
+        // Checking if two actions were used
+        if (actions == 2)
+        {
+            if(game.player == Players.PLAYER1)
+            {
+                game.turn = BattleSystem.HEROES;
+                game.heroesTurn();
+                actions = 0;
+            }
+            else if(game.player == Players.PLAYER2)
+            {
+                game.turn = BattleSystem.NAZGUL;
+                game.nazgulTurn();
+                actions = 0;
+            }
+        }
+
+       if (previousPiece != null)
+        {
+            foreach(Transform child in previousPiece.transform)
+            {
+                if(child.tag == "TileSelected" || child.tag == "Tile_Movement")
+                {
+                    child.gameObject.SetActive(false);
+                    foreach(Transform child2 in child.transform)
+                    {
+                        child2.gameObject.SetActive(false);
+                        foreach (Transform child3 in child2.transform)
+                        {
+                            child2.gameObject.SetActive(false);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Selecting pieces
+        if (objSelected.tag == "Pieces")
+            {
+
+                // Loop checking children and executign properly actions
+                foreach(Transform child in objSelected.transform)
+                {
+                    //Showing up tile selected
+                    if(child.tag == "TileSelected")
+                    {
+                        child.gameObject.SetActive(true);
+                    }
+                    //Showing up the tiles for movement
+                    if (child.tag =="Tile_Movement")
+                    {
+
+
+                    //Checking with position is inside the board
+                    if (validMovement(child))
+                    {
+                        //Getting children position in relation to the board
+                        int childXPosition = game.xPos(child);
+                        int childYPosition = game.yPos(child);
+
+                        //Children are dividided by levels. A Upper level is parent to a lower level.
+                        //Showing up tile movements if tile is empty
+                        if (emptySpace(childXPosition, childYPosition))
+                        {
+                            child.gameObject.SetActive(true);
+
+                            foreach (Transform child2 in child.transform)
+                            {
+                                if (validMovement(child2))
+                                {
+                                    childXPosition = game.xPos(child2);
+                                    childYPosition = game.yPos(child2);
+
+                                    //Checking second level of children
+                                    if (emptySpace(childXPosition, childYPosition))
+                                    {
+                                        child2.gameObject.SetActive(true);
+
+                                        //Checking third level of children
+                                        foreach (Transform child3 in child2.transform)
+                                        {
+                                            if (validMovement(child3))
+                                            {
+                                                childXPosition = game.xPos(child3);
+                                                childYPosition = game.yPos(child3);
+
+                                                //Testing                                                    
+                                                //Debug.LogError("Position X" + childXPosition + "Y:" + childYPosition);
+
+
+                                                if (emptySpace(childXPosition, childYPosition))
+                                                {
+                                                    child3.gameObject.SetActive(true);
+                                                }
+                                                else
+                                                {
+                                                    child3.gameObject.SetActive(false);
+                                                }
+
+                                            }
+                                            else
+                                            {
+                                                child3.gameObject.SetActive(false);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //    //Testing printing positions
+            //    foreach(GameObject testt in game.positions)
+            //{
+            //    if(testt != null)
+            //    {
+            //        Debug.LogError(testt.ToString() + "GB name : " +game.positions[9,4]);
+            //    }
+
+            //}
+
+
+                //Saving last piece clicked
+                previousPiece = objSelected;
+            }
+
+        //Selecting Tiles for movement
+        if (objSelected.tag == "Tile_Movement" && previousPiece != null)
+            {
+
+            int xPrevPosition = game.xPos(previousPiece.transform);
+            int yPrevPosition = game.yPos(previousPiece.transform);
+
+            //Testing
+            //Debug.LogError("Previous: "+previousPiece.ToString());
+            //Debug.LogError("ObjSelected:"+objSelected.ToString());
+
+
+            //TODO: User selects a Tile_Movement
+            //TODO: previous Position[x,y] is set to null
+            //TODO: new Position[x,y] is set
+            //TODO: TileSelected and TileMovement are destroyed
+            //TODO: Action is increased
+
+            //Set new Position
+            previousPiece.transform.position = new Vector2(objSelected.transform.position.x,objSelected.transform.position.y);
+
+            //Clean Movement
+            //cleanPathMovement(previousPiece.transform);
+
+            //Previous position is set to empty and new position is added
+            game.positions[xPrevPosition, yPrevPosition] = null;
+            game.positions[objXposition, objYposition] = previousPiece;
+
+
+
+
+
+
+            actions++;
+
+        }
+
+    }
+
+    //Checking if Position is valid (inside the board)
+    public bool validMovement(Transform obj)
+    {
+        if (obj.transform.position.x >= -6.5f && obj.transform.position.x <= 6.5f && obj.transform.position.y >= -6.5f && obj.transform.position.y <= 6.5f)
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Checking if position is empty
+    public bool emptySpace(int xPos, int yPos)
+    {
+        int x = xPos;
+        int y = yPos;
+
+        //testing
+        Debug.LogError("POSITION X:" + x + " Y: " + y);
+        Debug.LogError("Empty Poistion:"+ game.positions[x, y]);
+
+        if (game.positions[x,y] == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
+}
