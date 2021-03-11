9451552ea7e92e6d80e04e3a54d93d2f2706191e
EvandroGomezQuintino <36605703+EvandroGomezQuintino@users.noreply.github.com>
2021-03-11 00:45:09 +0000

    Finished GameOver system

---
+++ LOTR Chess/Software Development Project - LOTR Chess/Assets/Game/Scripts/gameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour
+{
+   
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
